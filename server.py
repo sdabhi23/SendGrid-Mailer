3b774f2b7d77ea06d9aca7cd20ce985eb7594e74
sdabhi23 <shrey.dabhi23@gmail.com>
2021-02-13 23:05:10 +0530

    turn off redoc endpoint

--- server.py
+++ server.py
@@ -1,11 +1,35 @@
+"""
+MIT License
+
+Copyright (c) 2018 Shrey Dabhi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from fastapi import FastAPI, Form, Header, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from sendgrid.helpers.mail import Mail
 import sendgrid
 import os
 
-app = FastAPI(title="E-Mail API", description="An API which uses SendGrid to send me mails about the forms submitted on my website!", version="1.0.0",
-              docs_url=None, redoc_url="/docs")
+app = FastAPI(title="E-Mail API", description="An e-mail sending API for static sites using SendGrid and Python on Heroku. It now requires setting a safety token inorder to avoid unauthorised usage and spam messages.",
+              version="1.0.0", docs_url=None, redoc_url=None)
 
 apikey = os.environ['SENDGRID_KEY']
 sg = sendgrid.SendGridAPIClient(apikey)
